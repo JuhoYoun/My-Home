ef9ec2eac716455140951ede27964128a2372f6c
Juho Youn <juho.youn.work@gmail.com>
2023-04-04 09:55:26 -0700

    Lecture 22

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from accountapp.views import hello_world, AccountCreateView
@@ -25,4 +26,7 @@ app_name = "accountapp"
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
     path('create/', AccountCreateView.as_view(), name='create'), # CSV를 가져와 쓸 때는 .as_view()를 해줘야한다.
+    # AccountCreateView는 views.py 에 선언된것을 가져왔다면 LoginView와 LogoutView는 장고에서 제공하는것을 가져왔다. 그래서 template_name을 여기서 지정한다.
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
