cdfbf01c14ef41548a64ec175db3c5277bdf3d94
Juho Youn <juho.youn.work@gmail.com>
2023-04-05 23:58:09 -0700

    Lecture 29

--- pinterest/settings.py
+++ pinterest/settings.py
@@ -137,6 +137,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# Media Setting
+MEDIA_URL = "media/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 LOGIN_REDIRECT_URL = reverse_lazy('accountapp:hello_world') # Class Based View 의 인자이므로 revese_lazy를 사용한다.
 LOGOUT_REDIRECT_URL = reverse_lazy('accountapp:login')
 
