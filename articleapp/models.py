8fbf2399c10ba90d9937bedad450cc8a53980933
Juho Youn <juho.youn.work@gmail.com>
2023-04-07 20:32:32 -0700

    Lecture 35

--- articleapp/models.py
+++ articleapp/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Article(models.Model):
+    writer = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='article', null=True) # on_delete=models.SET_NULL 유저가 탈퇴했을 시에 게시글을 없애지 않고 게시글의 유저만 null로 바꾼다 이때 null=True도 설정해줘야한다
+
+    title = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='article/', null=False)
+    content = models.TextField(null=True)
+
+    created_at = models.DateField(auto_created=True, null=True)
