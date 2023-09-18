273c3bf0066f81a0e6dd4f38aa0c84e38f3ca63d
Juho Youn <juho.youn.work@gmail.com>
2023-09-18 11:38:58 -0400

    Javascript image size  validator added in project create page

--- projectapp/forms.py
+++ projectapp/forms.py
@@ -1,9 +1,11 @@
 from django.forms import ModelForm
-
+from django import forms
 from projectapp.models import Project
 
 
 class ProjectCreationForm(ModelForm):
+    image = forms.ImageField(widget=forms.ClearableFileInput(attrs={'id': 'image-upload'}))
+
     class Meta:
         model = Project
         fields = ['image', 'title', 'description']
